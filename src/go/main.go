76136187e08cef17df80dbcacb2f17caa6adf71f
Sergio Méndez <sergio.mendez@yalochat.com>
2021-06-04 22:04:39 -0600

    Mod main.go

--- src/go/main.go
+++ src/go/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -14,16 +15,31 @@ func url_root(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`{"message": "OK"}`))
 }
 
+type Response struct {
+	N1 string
+	N2 string
+	Op string
+}
+
 func processData(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`"output": { "health":"OK"}`))
+
 	n1 := mux.Vars(r)["n1"]
 	fmt.Printf(n1)
-	var1 := mux.Vars(r)["n2"]
-	fmt.Printf(var1)
+	n2 := mux.Vars(r)["n2"]
+	fmt.Printf(n2)
 	op := mux.Vars(r)["op"]
 	fmt.Printf(op)
+
+	response := Response{n1, n2, op}
+	fmt.Printf(response.N1, response.Op)
+
+	js, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
 
 func main() {
