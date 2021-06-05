506cdd5828d74713ee4d881dcb1fe2d269af60a0
Sergio Méndez <sergio.mendez@yalochat.com>
2021-06-04 20:59:33 -0600

    added basic Python API

---
+++ src/python/index.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def url_root():
+    return "OK"
+
+
+
+@app.route("/bitacora/n1/<int:n1>/n2/<int:n2>/op/<op>", methods=["GET"])
+def writeBitacora(n1,n2,op):
+    print(n1,n2,op)
+    data = { 
+        "n1":n1,
+        "n2":n2,
+        "op":op
+    }
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
